fca5b05359eb3e4d0ad25821e87e4335ca43d3a2
EliveltonX2 <elivelton.pardini@gmail.com>
2026-02-01 16:38:55 -0400

    Removendo credenciais hardcoded e usando variaveis de ambiente

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ python manage.py migrate
 
 # --- NOVO COMANDO MÁGICO ---
 # Cria o superusuário automaticamente se ele não existir
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@email.com', 'MundoKaizo2026') if not User.objects.filter(username='admin').exists() else print('Admin já existe')" | python manage.py shell
+echo "import os; from django.contrib.auth import get_user_model; User = get_user_model(); username = os.environ.get('ADMIN_USERNAME', 'admin'); email = os.environ.get('ADMIN_EMAIL', 'admin@example.com'); password = os.environ.get('ADMIN_PASSWORD'); User.objects.create_superuser(username, email, password) if password and not User.objects.filter(username=username).exists() else print('Superuser check complete')" | python manage.py shell
