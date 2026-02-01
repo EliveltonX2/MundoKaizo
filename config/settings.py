3d057b1bac93782361fb055588127e84a1e548a8
EliveltonX2 <elivelton.pardini@gmail.com>
2026-02-01 15:38:13 -0400

    Ativando DEBUG para investigar erro 500

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-chave-padrao-desenvolvimento')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['*'] # Permite que o Render acesse o site
 
