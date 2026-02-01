63893c119a2c31de236c4388d2d2378a3eae98d0
EliveltonX2 <elivelton.pardini@gmail.com>
2026-02-01 15:08:50 -0400

    Script para criar superusuario automatico

--- build.sh
+++ build.sh
@@ -2,11 +2,15 @@
 # Sair se der erro
 set -o errexit
 
-# Instalar dependências
+# 1. Instalar dependências
 pip install -r requirements.txt
 
-# Coletar arquivos estáticos (CSS/JS)
+# 2. Coletar arquivos estáticos
 python manage.py collectstatic --no-input
 
-# Aplicar migrações no banco de dados
+# 3. Aplicar migrações (Cria as tabelas)
 python manage.py migrate
+
+# --- NOVO COMANDO MÁGICO ---
+# Cria o superusuário automaticamente se ele não existir
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@email.com', 'MundoKaizo2026') if not User.objects.filter(username='admin').exists() else print('Admin já existe')" | python manage.py shell
