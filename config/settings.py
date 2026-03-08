d2744b78010bce81dc83f9eec3f24c89950da566
EliveltonX2 <elivelton.pardini@gmail.com>
2026-03-07 22:30:49 -0400

    correcao de dotenv

--- config/settings.py
+++ config/settings.py
@@ -12,8 +12,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from dotenv import load_dotenv
-load_dotenv() # Carrega as variáveis do arquivo .env para o os.environ
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
